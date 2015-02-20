7305943f41abdc30b415b9711fb1f626ce6fea28
Tobias Frey <tobias@fre-y.de>
2015-02-20 20:05:12 +0100

    better infrastructure

--- scripts/bta.sh
+++ scripts/bta.sh
@@ -7,17 +7,19 @@ mkdir -p tmp
 cd tmp
 
 echo "# Compiling the infrastructure ..."
-clang ../impala/test/infrastructure/call_impala_main.c -O2 -c
+#clang ../impala/test/infrastructure/call_impala_main.c -O2 -c
+clang ../tests/infrastructure/call_impala_main.c -O2 -c
 
 echo "# Running impala on file $1 ..."
-impala --emit-llvm -bta ../"$1"
+impala --emit-llvm -Othorin ../"$1"
 
 echo "# Compiling program with infrastrcuture ..."
 clang "$name".bc call_impala_main.o
 
 echo "# Running the compiled program ..."
-./a.out
+./a.out $2 $3
+echo ""
 
-echo "-> return code was ($?)"
+#echo "-> return code was ($?)"
 
 cd ..
---
+++ tests/infrastructure/call_impala_main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main_impala(int a, int b);
+
+void* thorin_malloc(size_t size) { 
+    void* p;
+    posix_memalign(&p, 64, size);
+    return p;
+}
+
+void println(const char* s) {
+    printf("%s\n", s);
+}
+
+int main(int argc, const char* argv[]) {
+    // main_impala returns bool => 1 if everything ok, 0 if not
+    int a = -1;
+    int b = -1;
+
+    if(argc > 1) {
+        a = atoi(argv[1]);
+	if(argc > 2) {
+            b = atoi(argv[2]);
+        }
+    }
+        
+    printf("-> return code was (%i)\n", main_impala(a, b));
+
+    return 0;
+}
