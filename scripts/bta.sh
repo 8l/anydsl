a0fc28bd8103689a262d41e4ac1639bc2a6d682e
Tobias Frey <tobias@fre-y.de>
2015-02-20 18:11:46 +0100

    improved script

--- scripts/bta.sh
+++ scripts/bta.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -e
 
 filename=$(basename "$1")
 name=`echo $filename | cut -f1 -d'.'`
@@ -14,3 +14,10 @@ impala --emit-llvm -bta ../"$1"
 
 echo "# Compiling program with infrastrcuture ..."
 clang "$name".bc call_impala_main.o
+
+echo "# Running the compiled program ..."
+./a.out
+
+echo "-> return code was ($?)"
+
+cd ..
