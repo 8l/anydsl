93ba5ff4b749c338875df4e233c0dcb6655bea94
Tobias Frey <tobias@fre-y.de>
2015-02-26 19:38:26 +0100

    added some tests and improved scripts

--- scripts/bta.sh
+++ scripts/bta.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -e
+#!/bin/bash -e
 
 filename=$(basename "$1")
 name=`echo $filename | cut -f1 -d'.'`
@@ -10,14 +10,25 @@ echo "# Compiling the infrastructure ..."
 #clang ../impala/test/infrastructure/call_impala_main.c -O2 -c
 clang ../tests/infrastructure/call_impala_main.c -O2 -c
 
-echo "# Running impala on file $1 ..."
-impala --emit-llvm -Othorin ../"$1"
+if [ $2 == "-bta" ]; then
+	echo "# Running impala (only bta) on file $1 ..."
+	impala --emit-llvm -bta ../"$1"
+	
+	arg_a="$3"
+	arg_b="$4"
+else
+        echo "# Running impala on file $1 ..."
+        impala --emit-llvm -Othorin ../"$1"
+        
+	arg_a="$2"
+        arg_b="$3"
+fi
 
 echo "# Compiling program with infrastrcuture ..."
 clang "$name".bc call_impala_main.o
 
 echo "# Running the compiled program ..."
-./a.out $2 $3
+./a.out "$arg_a" "$arg_b"
 echo ""
 
 #echo "-> return code was ($?)"
